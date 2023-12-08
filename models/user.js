7a10c96300d43ea23e6f77e402398733afe7ef33
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-12-08 19:32:18 +0800

    Initial Commit

---
+++ models/user.js
@@ -0,0 +1,10 @@
+// import mongoose from "mongoose";
+// const Schema = mongoose.Schema;
+
+// const UserSchema = new Schema({
+// 	username: {},
+// 	password: {},
+// 	email: {},
+// 	member: {},
+// 	join_date: {},
+// });
